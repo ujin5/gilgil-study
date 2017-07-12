6253aa2d60574dfa248a6eefa1b06ce3fc5e036d
pwn2expoit <mashirogod@gmail.com>
2017-07-12 20:04:38 +0900

    issue001

--- sum_test/sum.c
+++ sum_test/sum.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-
-int main(){
-  int res = ((100+1)100)/2; // n(n+1)/2
+int main(int argc, char **argv, char **envp) {
+  int res = ((100+1)*100)/2; // n(n+1)/2
   printf("%d\n",res);
+  return 0;
 }
